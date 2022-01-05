3884181df6c8970f4aef720fdb90d07c271da798
potul <shrinath.potul@here.com>
2022-01-05 16:16:32 +0530

    adding summary page Collectors- Venkat

---
+++ src/main/java/com/collectors_venkat/README_summary.md
@@ -0,0 +1,28 @@
+        //Summary
+        //Collectors Venkat
+
+        //reduce -> sum, max, min, reduce, collect
+
+        //collect(Collector)
+
+        //Collectors
+
+        //toList, toMap, toSet
+        //toUnmodifieableList, toUnmodifieableMap, etc
+
+        //partitioningBy
+
+        //groupingBy(Function<T, R>)
+        //groupingBy(Function, Collector)
+        //mapping(Function, Collector)
+
+        //collectingAndThen(Collector, Function)
+
+        //Check TestCollector_ 5 examples, collectingAndThen(maxBy)
+        //toMap(Function keyMapper, Function valueMapper, BinaryOperator)
+
+        //teeing(Collector, Collector, Operator)
+
+        //flatMap(Function<T, Stream<R>>)
+
+        //flatMapping(Function<T, Stream<R>>)
--- src/main/java/com/collectors_venkat/TestCollector_8.java
+++ src/main/java/com/collectors_venkat/TestCollector_8.java
@@ -42,5 +42,40 @@ public class TestCollector_8 {
                         flatMapping(person -> Stream.of(person.getName().split("")), toList())));
         System.out.println(stringMap);
 
+        //Example 28
+        //getting all uppercase characters in the names of same ages
+        System.out.println(createPeople().stream()
+                .collect(groupingBy(Person::getAge,
+                        mapping(person -> person.getName().toUpperCase(),
+                        flatMapping(name -> Stream.of(name.split("")), toList())))));
+
+
+        //Summary
+        //reduce -> sum, max, min, reduce, collect
+
+        //collect(Collector)
+
+        //Collectors
+
+        //toList, toMap, toSet
+        //toUnmodifieableList, toUnmodifieableMap, etc
+
+        //partitioningBy
+
+        //groupingBy(Function<T, R>)
+        //groupingBy(Function, Collector)
+        //mapping(Function, Collector)
+
+        //collectingAndThen(Collector, Function)
+
+        //Check TestCollector_ 5 examples, collectingAndThen(maxBy)
+        //toMap(Function keyMapper, Function valueMapper, BinaryOperator)
+
+        //teeing(Collector, Collector, Operator)
+
+        //flatMap(Function<T, Stream<R>>)
+
+        //flatMapping(Function<T, Stream<R>>)
+
     }
 }
