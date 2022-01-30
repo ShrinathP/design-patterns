37e8f24db04062d7214e6a287b5342493c97eb86
potul <shrinath.potul@here.com>
2022-01-30 19:51:37 +0530

    lld

---
+++ src/main/java/com/lowLevelDesign_soumyajit/One_HotelManagementSystem/A.java
@@ -0,0 +1,11 @@
+package com.lowLevelDesign_soumyajit.One_HotelManagementSystem;
+
+public abstract class A {
+    protected abstract void m1();
+}
+
+class Test extends A {
+    @Override
+    public void m1() {
+    }
+}
